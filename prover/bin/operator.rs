9cb163169ab10c0e35d4f0802fab84d8c02bf36b
jonas pauli <jonaspauli089@gmail.com>
2025-04-24 16:41:56 +0200

    add optimization

--- prover/bin/operator.rs
+++ prover/bin/operator.rs
@@ -11,12 +11,14 @@ async fn main() {
     let tendermint_rpc_client = TendermintRPCClient::default();
     let prover = TendermintProver::new();
     let trusted_block_height: u64 = 28220389;
+    let target_block_height: u64 = 28220390;
     if trusted_block_height == 0 {
         panic!("No trusted height found on the contract. Something is wrong with the contract.");
     }
+
     let chain_latest_block_height = tendermint_rpc_client.get_latest_block_height().await;
     let (trusted_light_block, target_light_block) = tendermint_rpc_client
-        .get_light_blocks(trusted_block_height, chain_latest_block_height)
+        .get_light_blocks(trusted_block_height, target_block_height)
         .await;
     // Generate a proof of the transition from the trusted block to the target block.
     let proof_data = prover.generate_tendermint_proof(&trusted_light_block, &target_light_block);
